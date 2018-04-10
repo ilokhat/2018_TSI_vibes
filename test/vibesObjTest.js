192eb345e4ada05aad68742fb2df457345c564dc
ludivineschlegel <ludivine.sylvie@hotmail.fr>
2018-04-10 13:46:35 +0200

    ajustement test unitaires

--- test/vibesObjTest.js
+++ test/vibesObjTest.js
@@ -1,65 +1,59 @@
 /* global itowns, describe, it */
 var chai = require('chai');
 
+var parts;
+
 var manager = itowns.LayerManager.prototype;
 var symbolizer = itowns.Symbolizer.prototype;
 
 var f = new File([''], 'filename.text', { type: 'text/plain' });
 
 describe('LayerManager._readFile()', function () {
-  it('Expected .obj or .gibes file', function () {
-    chai.expect( function(){ manager._readFile(f); } ).to.throw("Unvalid format");
-  });
+    it('Expected .obj or .gibes file', function () {
+        chai.expect(function () { manager._readFile(f); }).to.throw('Unvalid format');
+    });
 });
 
- parts = [
-  new Blob(['v 0.123 0.234 0.345 1.0'], {type: 'text/plain'}),
-
-  new Uint16Array([33])
+parts = [
+    new Blob(['v 0.123 0.234 0.345 1.0'], { type: 'text/plain' }),
+    new Uint16Array([33]),
 ];
-var f1 = new File(parts, 'file.obj') ;
+var f1 = new File(parts, 'file.obj');
 
 describe('LayerManager._readFile()', function () {
-  it(".obj file loaded successfully", function () {
-    chai.expect(  manager._readFile(f1) ).to.equal(0);
-
-  });
+    it('.obj file loaded successfully', function () {
+        chai.expect(manager._readFile(f1)).to.equal(0);
+    });
 });
 
 
 parts = [
-  new Blob(['{"name":"croutitower","coordX":4202010,"coordY":178050,"coordZ":4779009,"rotateX":-3.093,"rotateY":0.851,"rotateZ":-1.634,"scale":300}'], {type: 'text/plain'}),
-
-  new Uint16Array([33])
+    new Blob(['{"name":"croutitower","coordX":4202010,"coordY":178050,"coordZ":4779009,"rotateX":-3.093,"rotateY":0.851,"rotateZ":-1.634,"scale":300}'], { type: 'text/plain' }),
+    new Uint16Array([33]),
 ];
-var f2 = new File(parts, 'sample.obj') ;
+var f2 = new File(parts, 'sample.obj');
 
 describe('LayerManager._readFile()', function () {
-  it(".gibes file loaded successfully", function () {
-    chai.expect(  manager._readFile(f2) ).to.equal(0);
-
-  });
+    it('.gibes file loaded successfully', function () {
+        chai.expect(manager._readFile(f2)).to.equal(0);
+    });
 });
 
-var folder ;
+var folder;
 
 describe('symbolizer.readVibes()', function () {
-  it("Expected .vibes file ", function () {
-    chai.expect( function(){ symbolizer._readVibes(f,folder); } ).to.throw("Unvalid format");
-
-  });
+    it('Expected .vibes file ', function () {
+        chai.expect(function () { symbolizer._readVibes(f, folder); }).to.throw('Unvalid format');
+    });
 });
 
-
- parts = [
-  new Blob(['{"edges":{"opacity":0.4124241147981423,"color":0,"width":5},"faces":[{"name":"Tower1_Tower_Fence_posts_Tube_4","opacity":0.5836138769686149,"color":4947287,"emissive":4428373,"specular":4428373,"shininess":30,"texture":null}]}'], {type: 'text/plain'}),
-
-  new Uint16Array([33])
+parts = [
+    new Blob(['{"edges":{"opacity":0.4124241147981423,"color":0,"width":5},"faces":[{"name":"Tower1_Tower_Fence_posts_Tube_4","opacity":0.5836138769686149,"color":4947287,"emissive":4428373,"specular":4428373,"shininess":30,"texture":null}]}'], { type: 'text/plain' }),
+    new Uint16Array([33]),
 ];
-var f3 = new File(parts, 'sample.vibes') ;
+var f3 = new File(parts, 'sample.vibes');
 describe('symbolizer.readVibes()', function () {
-  it(".vibes file loaded successfully", function () {
-    chai.expect(   symbolizer._readVibes(f3,folder) ).to.equal(0);
-
-  });
+    it('.vibes file loaded successfully', function () {
+        chai.expect(symbolizer._readVibes(f3, folder)).to.equal(0);
+    });
 });
