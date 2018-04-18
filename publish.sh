a7d99833948f3832bf9ff8b53fe7299aeeb2439a
ludivineschlegel <ludivine.sylvie@hotmail.fr>
2018-04-18 14:40:04 +0200

    passage CI to CI/CD 4

--- publish.sh
+++ publish.sh
@@ -5,10 +5,10 @@ NC='\033[0m'
 GREEN='\033[0;32m'
 LPURPLE='\033[1;35m'
 echo -e "${LPURPLE}Copie${NC} des ${BLUE}fichiers ${RED}arnaudgregoire/vibes${NC} sur ${RED}itownsResearch/2018_TSI_vibes${NC} ..."
-git push -f https://github.com/itownsResearch/2018_TSI_vibes master:master
+git push -f https://github.com/itownsResearch/2018_TSI_vibes.git master:master
 cd ..
 echo -e "${BLUE}Clonage${NC} du ${GREEN}dépot ${RED}itownsResearch/2018_TSI_vibes${NC} ..."
-git clone https://github.com/itownsResearch/2018_TSI_vibes
+git clone https://github.com/itownsResearch/2018_TSI_vibes.git
 cd 2018_TSI_vibes
 echo -e "${BLUE}Installation${NC} des ${LPURPLE}dépendances${NC} et ${LPURPLE}compilation${NC} des ${GREEN}fichiers${NC} (dossier ${RED}dist) ${NC}..."
 npm install
